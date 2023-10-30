dbcc5755d3f1a9e22019000579c494492875c1f1
shojikaori <149245814+shojikaori@users.noreply.github.com>
2023-10-31 08:06:12 +0900

    2nd push

--- main.py
+++ main.py
@@ -1,78 +1,52 @@
-import streamlit as st # フロントエンドを扱うstreamlitの機能をインポート
-import time # 時間を扱う機能をインポート
+import streamlit as st
+import requests
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
-st.title("streamlitの基礎") # タイトルが出力される
-st.write("hello world") # hello worldが出力される
+st.title('沖縄那覇市のホテル') # タイトル
 
-# レイアウトとして２列を定義
-col1, col2 = st.columns(2)
+st.write('DataFrame') # テキストの表示
 
-# 1列目をwithで囲む
-with col1:
-    st.write("列1がここに表示されます")
+#楽天APIからデータをとってくる
+REQUEST_URL="https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?"
+APP_ID="1058887096118834280" 
 
-# 2列目をwithで囲む
-with col2:
-    st.write("列2がここに表示されます")
+params = {
+    "format":"JSON",
+    "largeClassCode": "japan",
+    "middleClassCode": "okinawa",
+    "smallClassCode": "nahashi",
+    "applicationId": APP_ID
+}
 
+res = requests.get(REQUEST_URL, params)
 
+result = res.json()
+# これでAPIで取得したデータはresultに代入されます。
 
-st.sidebar.write("hello world") #.sidebar付けるとサイトバーに出力されます。
-st.text_input("ここに文字が入力できます。") # テキストを入力できます。
+df = pd.DataFrame()
 
-slider_text = st.slider("スライダーで数字を決定できます。",0,100,5) # (最小、最大値、デフォルト値)の順で設定されます。
-"スライダーの数字:",slider_text
+i=0
+hotel_info = hotel_info = result["hotels"][i]["hotel"][0]["hotelBasicInfo"]
 
-st.button("ボタン") # ボタンが設置されます。
+for i in range(0, len(result["hotels"])):
+    hotel_info = result["hotels"][i]["hotel"][0]["hotelBasicInfo"]
+    temp_df = pd.DataFrame(hotel_info, index=[i])
+    df = pd.concat([df, temp_df])
 
-x = st.empty() # 文字が出力される場所をあらかじめ確保します。その場所をxとしています。
-bar = st.progress(0) # 進捗0のプログレスバーを出力します。
+df[['hotelMinCharge', 'reviewAverage']]
 
-# iに0から99まで代入しながら実行されます
-for i in range(100):
-    time.sleep(0.1) # 0.1秒待機します。
-    x.text(i) # 確保した場所xに代入されたiの値を代入します。
-    bar.progress(i) # 進捗iに変更します。
-    i += 1 # iに1足し算して代入します。
+#Streamlitでplotyでグラフ化
 
-# 選択肢を配列で指定して選択肢を出力します。
-st.selectbox("選んでください。",["選択肢1","選択肢2","選択肢3"])
+st.subheader("那覇市のホテルのレビューと価格の関係")
+fig = px.scatter(
+    df,
+    x="hotelMinCharge",
+    y="reviewAverage",
+    color="reviewAverage",
+    color_continuous_scale="reds",
+)
 
+st.plotly_chart(fig)
 
-
-# ダウンロードする文字を定義し、output_textに代入します。
-output_text = "この文字がダウンロードされます"
-
- # 代入された文字をダウンロードするボタンを設置。オプションは内容をdataに指定、ファイル名をfile_nameに指定、ファイルタイプをmimeに指定
-st.download_button(label='記事内容 Download', 
-                   data=output_text, 
-                   file_name='out_put.txt',
-                   mime='text/plain',
-                   )
-
-
-# ファイルアップローダーを設置します。typeでアップロードできるファイルの種類を指定できます。
-file_upload = st.file_uploader("ここに音声認識したファイルをアップロードしてください。",type=["png","jpg"])
-
-# ファイルがアップロードされた時にその画像を表示します。
-if (file_upload !=None):
-    st.image(file_upload)# 画像を表示します。
-
-
-
-import numpy as np # 数列を扱う機能をインポート
-import pandas as pd # データフレームを扱う機能をインポート
-
-# 乱数の配列を作るメソッドを作ります。引数r,cとし、それぞれおのデフォルト値を10と5に設定します。
-def rand_df(r=10, c=5):
-    df = pd.DataFrame(
-        np.random.randn(r, c),
-        columns=('col %d' % i for i in range(c)))# 乱数10の５個の数列を作ります。カラムの設定は0-4の名前を付けます。
-    return df # 作ったデータフレームを返します。
-
-dataframe = rand_df(r=10,c=3) # rに10、cに3を代入したrand_dfメソッドを処理します。
-
-# 表の描画します。
-st.dataframe(dataframe.head(n=3))
-# データフレームのチャートの描画します。
-st.line_chart(dataframe)
